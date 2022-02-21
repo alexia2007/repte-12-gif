2bf6c8cc87cf957d314ec829e5b463918058a50f
alexia2007 <99951243+alexia2007@users.noreply.github.com>
2022-02-21 13:08:23 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,45 @@
+basic.showLeds(`
+    . # # # .
+    # # # # #
+    # # # # #
+    # # # # #
+    . # # # .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . # . .
+    . # # # .
+    . . # . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.showIcon(IconNames.Chessboard)
+basic.forever(function () {
+    basic.showLeds(`
+        # # . . .
+        . # . . .
+        . # . . .
+        . # # # #
+        . # . # .
+        `)
+    basic.showLeds(`
+        . # # . .
+        . # . . .
+        . # . . .
+        . # # # #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # # . . .
+        . # . . .
+        . # . . .
+        . # # # .
+        . # . # #
+        `)
+})
